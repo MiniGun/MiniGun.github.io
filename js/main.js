5d241985b45b84aabc3f8014986829af6b233b47
Jacque Bougie <jacqueboug@gmail.com>
2018-04-16 22:26:12 -0400

    speed

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
-$(".animate").animate({opacity: '1', top: '150px'});
+$(".animate").animate({opacity: '1', top: '150px'},700);
+$(".playercount").animate({opacity: '1', top: '150px'},800);
 $(".fade").hide().fadeIn(800,"swing");
