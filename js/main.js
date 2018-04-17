5cfaf920441ad767b0b781c120e0cd07df62046f
Jacque Bougie <jacqueboug@gmail.com>
2018-04-16 22:17:29 -0400

    test7

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-$(".animate").hide().animate({opacity: '1', top: '150px'});
+$(".animate").hide().animate({opacity: '1', top: '150px', display: 'block', width: '1000'});
 $(".fade").hide().fadeIn(800,"swing");
