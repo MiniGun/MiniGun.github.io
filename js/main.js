3891773f3181e82b55d0fa3076d2d133a0185527
Jacque Bougie <jacqueboug@gmail.com>
2018-05-11 16:46:23 -0400

    Update - New homepage animations part #1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-$(".animate").animate({opacity: '1', top: '150px'},700);
+$(".animate").animate({opacity: '1', left: '10px'},700);
 $(".playercount").animate({opacity: '1', top: '150px'},800);
 $(".fade").hide().fadeIn(800,"swing");
