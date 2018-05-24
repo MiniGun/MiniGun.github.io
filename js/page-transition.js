ea777d319e61ef18bb7fff539841dd9018e200b7
Jacque Bougie <jacqueboug@gmail.com>
2018-05-23 21:49:21 -0400

    Update - Lots of Changes

---
+++ js/page-transition.js
@@ -0,0 +1,39 @@
+$("#btn").click(function() {
+  $('.transition').toggleClass('transition-active');
+});
+
+$("#btn1").click(function() {
+  $('.transition').toggleClass('transition-active');
+});
+
+$("#btn2").click(function() {
+  $('.transition').toggleClass('transition-active');
+});
+
+$("#btn3").click(function() {
+  $('.transition').toggleClass('transition-active');
+});
+
+$('#btn').click(function(e) {
+  e.preventDefault();
+  var linkUrl = $(this).attr('href');
+  setTimeout(function(url) { window.location = '/home.html'; }, 1000, linkUrl);
+});
+
+$('#btn1').click(function(e) {
+  e.preventDefault();
+  var linkUrl = $(this).attr('href');
+  setTimeout(function(url) { window.location = '/home.html'; }, 1000, linkUrl);
+});
+
+$('#btn2').click(function(e) {
+  e.preventDefault();
+  var linkUrl = $(this).attr('href');
+  setTimeout(function(url) { window.location = '/staff.html'; }, 1000, linkUrl);
+});
+
+$('#btn3').click(function(e) {
+  e.preventDefault();
+  var linkUrl = $(this).attr('href');
+  setTimeout(function(url) { window.location = '/home.html'; }, 1000, linkUrl);
+});
