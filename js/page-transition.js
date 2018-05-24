8b828b353f83fad732918a17cfb9f459dd1ebcd4
Jacque Bougie <jacqueboug@gmail.com>
2018-05-23 21:52:58 -0400

    Update - Fixed JS

--- js/page-transition.js
+++ js/page-transition.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $("#btn").click(function() {
   $('.transition').toggleClass('transition-active');
 });
@@ -37,14 +36,4 @@ $('#btn3').click(function(e) {
   e.preventDefault();
   var linkUrl = $(this).attr('href');
   setTimeout(function(url) { window.location = '/home.html'; }, 1000, linkUrl);
-=======
-$("#btn").click(function() {
-  $('.transition').toggleClass('transition-active');
-});
-
-$('.btn').click(function(e) {
-  e.preventDefault();
-  var linkUrl = $(this).attr('href');
-  setTimeout(function(url) { window.location = url; }, 1000, linkUrl);
->>>>>>> c6402c435a5abc6fd92b660beb17b06f02c9e65b
 });
