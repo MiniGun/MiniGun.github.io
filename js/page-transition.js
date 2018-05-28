fa7f07f5ccdb5931c11005ac4d40bfeb1cf0290e
Jacque Bougie <jacqueboug@gmail.com>
2018-05-27 22:52:31 -0400

    Update - Future Plans Content and transitions

--- js/page-transition.js
+++ js/page-transition.js
@@ -1,17 +1,17 @@
 $("#btn").click(function() {
-  $('.transition').toggleClass('transition-active');
+  $('transition').toggleClass('transition-active');
 });
 
 $("#btn1").click(function() {
-  $('.transition').toggleClass('transition-active');
+  $('transition').toggleClass('transition-active');
 });
 
 $("#btn2").click(function() {
-  $('.transition').toggleClass('transition-active');
+  $('transition').toggleClass('transition-active');
 });
 
 $("#btn3").click(function() {
-  $('.transition').toggleClass('transition-active');
+  $('transition').toggleClass('transition-active');
 });
 
 $('#btn').click(function(e) {
@@ -36,4 +36,18 @@ $('#btn3').click(function(e) {
   e.preventDefault();
   var linkUrl = $(this).attr('href');
   setTimeout(function(url) { window.location = '/'; }, 1000, linkUrl);
+});
+
+/* Content Transition */
+$(".info").click(function() {
+  $('h').toggleClass('h-active');
+  $('p').toggleClass('p-active');
+  $('h1').toggleClass('h1-active');
+  $('p1').toggleClass('p1-active');
+  $('.playercount').toggleClass('playercount-active');
+
+  $('h2').toggleClass('h2-active');
+  $('p3').toggleClass('p3-active');
+  $('h3').toggleClass('h3-active');
+  $('p4').toggleClass('p4-active');
 });
